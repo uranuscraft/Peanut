65f9a853489abbb4d4eef5719c3d6ca442eebff2
uranuscraft <uranuscraft@gmail.com>
2015-04-21 19:13:48 -0400

    Update Peanut.ino

--- Peanut.ino
+++ Peanut.ino
@@ -12,7 +12,7 @@ void setup()
 
 {
  pinMode(switchPin, INPUT); // sets the pin as input
-  pinMode(analogPin, INPUT); // sets the pin as input
+ 
   pinMode(ledPin, OUTPUT);   // sets the pin as output
   pinMode(outPin, OUTPUT);   // sets the pin as output
 }
@@ -31,6 +31,20 @@ void loop()
 Calibration();
 cycles = cycles + 924;// the amount of cycles that have been gone through
 if(digitalRead(switchPin) == HIGH) {// if the switch is activated 
+  int Read = analogRead(analogPin);
+ int AvgOne = Mean[0] + Mean[1] + Mean[2] + Mean[3] + Mean[4] + Mean[5] + Mean[6] + Mean[7] + Mean[8] + Mean[9] + Mean[10];
+  int Avg = AvgOne / 10;
+   int SDOne = StDe[0] + StDe[1] + StDe[2] + StDe[3] + StDe[4] + StDe[5] + StDe[6] + StDe[7] + StDe[8] + StDe[9] + StDe[10];
+  int StD = SDOne / 10;
+  
+  int FourSD = StD * 4;
+   int NegFourSD = StD * -4;
+  if(Read > FourSD) {
+      digitalWrite(ledPin, HIGH);
+  }
+  if(Read < NegFourSD) {
+   digitalWrite(ledPin, HIGH);
+  }
   
 }
 else{
